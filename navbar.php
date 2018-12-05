72149d446cc734a2c8e389f7ce23ef292810466c
Antoine <thomas.antoine59@outlook.fr>
2018-12-05 09:33:26 +0100

    [html] Navbar

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="hidden-nav">
+                <div>
+                    <span>deja fait</span>
+                    <span>bro</span>
+                    <span>couscous</span>
+                </div>
+                <div>
+                    <span>dromadaire</span>
+                    <span>croisade</span>
+                    <span>blabla</span>
+                </div>
+                <div>
+                    <span>Lien</span>
+                    <span>jerusalem</span>
+                    <span>Youtube</span>
+                </div>
+            </div>
+            <div class="nav-bar">
+                <div>logo</div>
+                <div>menu</div>
+                <div>recherche</div>
+                <div>
+                    <span>recette</span>
+                    <span>plat du jour</span>
+                    <span>mardi gras</span>
+                </div>
+                <div>compte</div>
+            </div>
+      </nav>
+  </header>
+</body>
+</html>
