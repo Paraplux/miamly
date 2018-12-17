b8741e3e68c4f0d1dc6f592da13ae2277cf10506
Marc Bouchez <theparaplux@gmail.com>
2018-12-17 09:04:16 +0100

    17 décembre 2018

--- controllers/controller-recettes.php
+++ controllers/controller-recettes.php
@@ -5,4 +5,12 @@ $req->execute();
 $recettes =  $req->fetchAll();
 $req->closeCursor();
 
+
+$req = $pdo->prepare('SELECT * FROM mly_photos');
+$req->execute();
+foreach($recettes as $recette) {
+    $recette['photos'] = $req->fetchAll();
+
+}
+$req->closeCursor();
 ?>
--- views/sign.php
+++ views/sign.php
@@ -1,7 +1,27 @@
-<form method="POST" action="../actions/action-sign.php">
-    <input name="email" type="email" placeholder="Votre mail">
-    <input name="pseudo" type="text" placeholder="Pseudo">
-    <input name="password" type="password" placeholder="Mot de passe">
-    <input name="password_confirmation" type="password" placeholder="Confirmation de mot de passe">
-    <input type="submit">
-</form>
+<?php
+include '../components/header.php';
+include '../components/navbar.php';
+?>
+
+<link rel="stylesheet" href="../css/sign.css">
+
+<div class="container">
+    <section class="sign-page">
+        <form class="sign-form" method="POST" action="../actions/action-sign.php">
+            <input name="email" type="email" placeholder="Votre mail"><br>
+            <input name="pseudo" type="text" placeholder="Pseudo"><br>
+            <input name="password" type="password" placeholder="Mot de passe"><br>
+            <input name="password_confirmation" type="password" placeholder="Confirmation de mot de passe"><br>
+            <input type="submit"><br>
+        </form>
+        <article class="sign-content">
+            <h1>Blabla</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum odio ex reiciendis, culpa quos voluptate, expedita, corporis eveniet quia atque placeat necessitatibus tempore architecto officia nulla laudantium iste unde! Aliquid.
+            Ipsum totam doloribus, facilis vel sunt aut inventore officiis id a sint aliquam vero non numquam consequuntur optio esse voluptates rem sit. Maiores sit quis velit pariatur in? Quasi, quae.</p>
+        </article>
+    </section>
+</div>
+
+<?php 
+include '../components/footer.php';
+?>
