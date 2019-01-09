fba657002e56aff65ebbfab04fa12056b0adcb0a
Tyrael de la sainte bagarre <thomas.antoine59@outlook.fr>
2019-01-09 10:41:51 +0100

    xp bar

--- views/communaute.php
+++ views/communaute.php
@@ -6,17 +6,21 @@ session_start();
 
 include '../components/header.php';
 include '../components/navbar.php';
-include '../controllers/controller-recettes.php';
+// include '../controllers/controller-recettes.php';
 ?>
 <link rel="stylesheet" href="../css/card2.css">
 <br><br>
 <div class="container">
     
     <div class="recette-comm">
-        <div class="photo-comm"><img src="../images/thumbs/recettes/fondant.jpg"  class="imagedefou"></div>    
-        <div class="resume-comm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusantium error, fuga, reiciendis, eos excepturi commodi dolore enim architecto aliquid necessitatibus veritatis aut quis quae voluptatem? Sunt nesciunt repellat accusamus!</div>
-        <div class="note">Lorem ipsu Error esse natus fuga, perspiciatis illum placeat facilis quide.</div>
-        <div class="xp_bar">Lorem ipsum dolorrepellendus facilis nesciunt! Odit magni neque dolores hic non praesentium tenetur illo ipsam. In distinctio vel voluptates!</div>
+        <div class="photo-comm"><img src="../images/thumbs/recettes/fondant.jpg"  class="image_stats"></div>    
+        <div class="resume-comm"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusantium error, fuga, reiciendis, eos excepturi commodi dolore enim architecto aliquid necessitatibus veritatis aut quis quae voluptatem? Sunt nesciunt repellat accusamus!</p></div>
+        <div class="note"><p>Daniel</p></div>
+        <div class="xp_bar">
+        <div class="outer">
+  <div class="xp-bar" style="width:40%"></div>
+</div>
+        </div>
     </div>
 
 <?php 
