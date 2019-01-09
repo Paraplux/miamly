3f7557a81aed64224c484dc0a5ef48551a488f4d
Marc Bouchez <theparaplux@gmail.com>
2019-01-09 10:41:50 +0100

    Update home

--- components/header.php
+++ components/header.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Miamly</title>
 
-    <!-- JQUERY -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- EXTERNAL SCRIPTS -->
     <script src="../js/jquery.js"></script>
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
     
+    <!-- EXTERNAL CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
     <!-- PERSONNAL CSS -->
     <link rel="stylesheet/less" href="../css/app.less">
     <link rel="stylesheet/less" href="../css/navbar.less">
