54922952f714b06aa29dd1ae91e90fe0a67e87a2
Marc Bouchez <theparaplux@gmail.com>
2019-01-17 23:31:26 +0100

    Update sur la recherche, fix bug
    On ne pouvait pas changer la recherche dans la barre d'url, maintenant c'est fixé, on peut !

--- actions/action-search.php
+++ actions/action-search.php
@@ -1,15 +1,9 @@
 <?php 
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
 if(isset($_GET)) {
     if(empty($_GET['search'])) {
         header('Location: ../views/home.php');
     } else {
-        $search = explode(" ", $_GET['search']);
-        $_SESSION['search'] = $search;
         header('Location: ../views/catalogue?search='. str_replace(" ", "+", $_GET['search']));
     }
 }
--- models/model-catalogue.php
+++ models/model-catalogue.php
@@ -1,10 +1,10 @@
 <?php
-include("../components/db.php");
+include "../components/db.php";
+$sqlSearch = array(); // Stop errors when $words is empty
+$currentSearch = explode(" ", $_GET['search']);
 
-$search = array(); // Stop errors when $words is empty
-
-foreach ($_SESSION['search'] as $keyword) {
-        $search[] = '
+foreach ($currentSearch as $keyword) {
+        $sqlSearch[] = '
         r_nom SOUNDS LIKE "%' . $keyword . '%"
         OR r_nom LIKE "%' . $keyword . '%"
         OR r_content LIKE "%' . $keyword . '%"
@@ -19,7 +19,7 @@ foreach ($_SESSION['search'] as $keyword) {
 $sql = 'SELECT mly_recettes.r_id, mly_recettes.r_type, mly_recettes.r_nom, mly_recettes.r_content, mly_recettes.r_date, mly_recettes.r_createur, mly_photos.p_link
         FROM mly_recettes
         INNER JOIN mly_photos ON mly_recettes.r_id = mly_photos.p_recette_id
-        WHERE ' . implode(" OR ", $search) . '
+        WHERE ' . implode(" OR ", $sqlSearch) . '
         GROUP BY mly_recettes.r_id, mly_photos.p_link';
 
 
