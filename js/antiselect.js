38f36987831bd0cb2e3c470ee4810fc8b6c3a5fa
Tyrael de la sainte bagarre <thomas.antoine59@outlook.fr>
2019-01-21 13:51:15 +0100

    gilet jaune.js
    on ne bouge plus

---
+++ js/antiselect.js
@@ -0,0 +1,1 @@
+function ffalse() { return false; } function ftrue() { return true; } document.onselectstart = new Function ("return false"); if(window.sidebar) { document.onmousedown = ffalse; document.onclick = ftrue; }
