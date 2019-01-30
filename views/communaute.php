9ad479d0e7bb91f6154949683ba015a548cb4563
Tyrael de la sainte bagarre <thomas.antoine59@outlook.fr>
2019-01-30 11:15:25 +0100

    petits ajouts

--- views/communaute.php
+++ views/communaute.php
@@ -10,19 +10,11 @@ include '../components/navbar.php';
 ?>
 <link rel="stylesheet" href="../css/card2.css">
 <br><br>
-<div class="container">
-    
-    <div class="recette-comm">
-        <div class="photo-comm"><img src="../images/thumbs/recettes/fondant.jpg"  class="image_stats"></div>    
-        <div class="resume-comm"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusantium error, fuga, reiciendis, eos excepturi commodi dolore enim architecto aliquid necessitatibus veritatis aut quis quae voluptatem? Sunt nesciunt repellat accusamus!</p></div>
-        <div class="note"><p>Daniel</p></div>
-        <div class="xp_bar">
-        <div class="outer">
-  <div class="xp-bar" style="width:40%"></div>
+  <div class="img-recette"> </div>
+  <div class="nom-recette"> </div>
+  <div class="outer">
+  <div class="xp-bar" style="width:10%"></div>
 </div>
-        </div>
-    </div>
-
 <?php 
 include '../components/footer.php';
 ?>
